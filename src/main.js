13bd859acd73b4c6a346ded6d97f7f3192551949
Maycon Douglas <105015618+maycon-douglas157420@users.noreply.github.com>
2026-07-15 21:42:18 -0300

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Logger from "./core/Logger.js";
+import Version from "./core/Version.js";
+
+Logger.info(`${Version.NAME}`);
+
+Logger.info(`Versão ${Version.VERSION}`);
+
+Logger.info("Inicializando...");
