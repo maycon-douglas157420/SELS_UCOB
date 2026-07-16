56714270b6b66bdef7e05de2d29589a6be43b9fc
Maycon Douglas <105015618+maycon-douglas157420@users.noreply.github.com>
2026-07-15 21:38:16 -0300

    Create Constants.js

---
+++ src/core/Constants.js
@@ -0,0 +1,23 @@
+export const MODULES = {
+
+    DEMONSTRATIVE: "demonstrative",
+
+    FINANCIAL: "financial",
+
+    ORDERS: "orders",
+
+    CONSIGNMENT: "consignment"
+
+};
+
+export const STATUS = {
+
+    READY: "ready",
+
+    RUNNING: "running",
+
+    ERROR: "error",
+
+    SUCCESS: "success"
+
+};
